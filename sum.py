fa9c1233850534a6d3f4f6e2093a5eaa60d762bb
codyV <cvd97@ou.edu>
2023-09-12 12:39:34 -0500

    Added the reverse funtion to the main program.

--- sum.py
+++ sum.py
@@ -1,5 +1,9 @@
 numbers = [2,3,4,5,5]
 
+def reverse(numbers):
+    list = numbers[::-1]
+    return list
+
 def sum(numbers):
     total = 0
     for number in numbers:
@@ -25,5 +29,6 @@ def main():
     
     print(sum(n))
     print(multiply(n))
+    print(reverse(n))
     
 main()
